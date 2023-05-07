4a0f1e575f7d4cc63c43a9962d67ccf994ee7913
JimPyh <jorn.verwimp@gmail.com>
2023-05-07 13:13:20 +0200

    update draw using arguments

--- slither_draw.py
+++ slither_draw.py
@@ -1,6 +1,7 @@
 import drawsvg as draw
-
-input_str = "(1, 1, 1, 2), (1, 2, 2, 2), (1, 4, 1, 5), (1, 5, 1, 6), (1, 6, 2, 6), (2, 1, 1, 1), (2, 2, 2, 3), (2, 3, 3, 3), (2, 4, 1, 4), (2, 5, 3, 5), (2, 6, 2, 5), (3, 1, 2, 1), (3, 2, 3, 1), (3, 3, 3, 4), (3, 4, 2, 4), (3, 5, 4, 5), (4, 1, 4, 2), (4, 2, 3, 2), (4, 3, 5, 3), (4, 4, 4, 3), (4, 5, 4, 6), (4, 6, 5, 6), (5, 1, 4, 1), (5, 2, 6, 2), (5, 3, 5, 2), (5, 4, 4, 4), (5, 5, 5, 4), (5, 6, 6, 6), (6, 1, 5, 1), (6, 2, 6, 1), (6, 5, 5, 5), (6, 6, 6, 5)"
+import sys
+output_str = sys.argv[1]
+input_str = sys.argv[2]
 
 coords_str = input_str.replace('(', '').replace(')', '')
 coords_list = coords_str.split(', ')
@@ -16,7 +17,7 @@ for i in range(0, len(coords_list), 4):
 
 d.set_pixel_scale(2)
 
-d.save_svg('arrows2.svg')
+d.save_svg(output_str)
 
 
 
